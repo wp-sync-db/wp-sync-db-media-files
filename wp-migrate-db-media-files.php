8114a350543356be1737d4090d54644b23d450ec
Sean Lang <slang800@gmail.com>
2013-12-30 16:38:26 -0600

    spelling & support github-updater
    also, the license is already in a separate file

--- wp-migrate-db-media-files.php
+++ wp-migrate-db-media-files.php
@@ -1,23 +1,12 @@
 <?php
 /*
 Plugin Name: WP Migrate DB Media Files
-Plugin URI: http://deliciousbrains.com/wp-migrate-db-pro/
-Description: An extension to WP Migrate DB, allows the migration of media files.
-Author: Delicious Brains
+Description: An extension of WP Migrate DB that allows the migration of media files.
+Author: Sean Lang
 Version: 1.0
-Author URI: http://deliciousbrains.com
+Author URI: http://slang.cx
+GitHub Plugin URI: slang800/wp-migrate-db-media-files
 */
-
-// Copyright (c) 2013 Delicious Brains. All rights reserved.
-//
-// Released under the GPL license
-// http://www.opensource.org/licenses/gpl-license.php
-//
-// **********************************************************************
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// **********************************************************************
 
 function wp_migrate_db_media_files_init() {
 	if ( ! is_admin() || ! class_exists( 'WPMDB_Addon' ) ) return;
