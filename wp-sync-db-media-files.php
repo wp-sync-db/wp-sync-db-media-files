5f2965a199da0c55008564c3a4e93439fab9053d
Sean Lang <slang800@gmail.com>
2016-03-07 13:13:00 -0500

    apply PSR1 & PSR2 formatting

--- wp-sync-db-media-files.php
+++ wp-sync-db-media-files.php
@@ -7,26 +7,32 @@ Version: 1.1.4b1
 Author URI: http://slang.cx
 GitHub Plugin URI: wp-sync-db/wp-sync-db-media-files
 Network: True
-*/
+ */
 
 require_once 'version.php';
-$GLOBALS['wpsdb_meta']['wp-sync-db-media-files']['folder'] = basename( plugin_dir_path( __FILE__ ) );
+$GLOBALS['wpsdb_meta']['wp-sync-db-media-files']['folder'] = basename(plugin_dir_path(__FILE__));
 
-function wp_sync_db_media_files_loaded() {
-	if ( ! class_exists( 'WPSDB_Addon' ) ) return;
+function wp_sync_db_media_files_loaded()
+{
+    if (!class_exists('WPSDB_Addon')) {
+        return;
+    }
 
-	require_once 'class/wpsdb-media-files.php';
+    require_once 'class/wpsdb-media-files.php';
 
-	global $wpsdb_media_files;
-	$wpsdb_media_files = new WPSDB_Media_Files( __FILE__ );
+    global $wpsdb_media_files;
+    $wpsdb_media_files = new WPSDB_Media_Files(__FILE__);
 }
 
-add_action( 'plugins_loaded', 'wp_sync_db_media_files_loaded', 20 );
+add_action('plugins_loaded', 'wp_sync_db_media_files_loaded', 20);
 
-function wp_sync_db_media_files_init() {
-	if ( ! class_exists( 'WPSDB_Addon' ) ) return;
+function wp_sync_db_media_files_init()
+{
+    if (!class_exists('WPSDB_Addon')) {
+        return;
+    }
 
-	load_plugin_textdomain( 'wp-sync-db-media-files', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    load_plugin_textdomain('wp-sync-db-media-files', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
 
-add_action( 'admin_init', 'wp_sync_db_media_files_init', 20 );
+add_action('admin_init', 'wp_sync_db_media_files_init', 20);
